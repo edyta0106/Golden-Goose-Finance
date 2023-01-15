f5a0ac9d3122ade15848cec6be74541c5f6643f5
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-15 12:55:14 -0700

    finished the coding the models in the server directory

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,4 @@
 const User = require("./User");
 const FinancialData = require("./FinancialData")
-// const TotalIncome = require("./TotalIncome")
-// const TotalBills = require("./TotalBills");
-// const TotalSavings = require("./TotalSavings");
-// const TotalSpending = require("./TotalSpending");
 
 module.export = { User, FinancialData };
