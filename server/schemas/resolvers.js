8b7ac5fe6b270a3f53d11795c0405780c5dc6f1c
MandieBot <amandabotinelly@gmail.com>
2023-01-16 12:14:39 -0700

    Typedefs and resolvers structured out

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,19 +1,37 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { FinancialData, User } = require("../models");
 const { signToken } = require("../utils/auth");
-// const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
-    Query: {
-        user: async (parent, args, context) => {
-            if(context.user) {
-                const user = await User.findById(context.user._id).populate({
-                    path: "savings.total",
-                    populate: ""
-                })
-            }
-            return User.find();
-        },
+  Query: {
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          populate: "finances",
+        });
+      }
+      return User.find();
+    },
+    Mutation: {
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
 
-    }
-}
+        if (!user) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+
+        const token = signToken(user);
+
+        return { token, user };
+      },
+    },
+  },
+};
+
+module.exports = resolvers;
