5fa4b1ebbf61a57684338a0dbd8d37f581eb953f
MandieBot <amandabotinelly@gmail.com>
2023-01-17 15:36:54 -0700

    User can now be created and login

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,16 +1,5 @@
 import { gql } from "@apollo/client";
 
-// export const ADD_USER = gql`
-//   mutation addUser($firstName: String!, $lastName: String!, $username: String!, $email: String!, $password: String!) {
-//     addUser(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password) {
-//       token
-//       user {
-//         _id
-//       }
-//     }
-//   }
-// `;
-
 export const ADD_USER = gql`
   mutation AddUser($username: String!, $password: String!, $email: String!) {
     addUser(username: $username, password: $password, email: $email) {
@@ -18,6 +7,17 @@ export const ADD_USER = gql`
         _id
         username
         password
+        email
+      }
+    }
+  }
+`;
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
         email
       }
     }
