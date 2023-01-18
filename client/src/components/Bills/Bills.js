feed1babf03506c0f56fbdc5727bf79ef3f87102
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-18 12:22:12 -0700

    cleaned up box shadows on the reusable form

--- client/src/components/Bills/Bills.js
+++ client/src/components/Bills/Bills.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Form from "../ReusableForm/form"
 
 export default function Bills() {
   return (
     <>
       <Link to="/bills"></Link>
+      <Form />
     </>
   );
 }
