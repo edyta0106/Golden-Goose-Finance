90683e6b5ce7d6e217bb1f44dca13569f382d2d6
MandieBot <amandabotinelly@gmail.com>
2023-01-18 12:59:12 -0700

    Working on Savings Page layout

--- client/src/components/Savings/Savings.js
+++ client/src/components/Savings/Savings.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import { Typography, Button, Container } from "@mui/material";
+import { textAlign } from "@mui/system";
+
+const dayjs = require("dayjs");
 
 export default function Savings() {
   return (
     <>
       <Link to="/savings"></Link>
-      <h1>savings</h1>
+
+      <Container sx={{ bgcolor: "#546E7A", height: "100vh" }} maxWidth="sm">
+        <Typography variant="h3" component="h1">
+          January 18, 2023 Savings Goals
+        </Typography>
+
+        {/* dayjs().format(MM/DD/YYYY) */}
+        <Box />
+        <Box textAlign="center" sx={{ p: 2, border: "3px solid black" }}>
+          <Button type="submit">Set a New Goal</Button>
+        </Box>
+      </Container>
     </>
   );
 }
