4111013e227d23c1076136068ab4ab144db76e84
92castro <celin92castro@gmail.com>
2023-01-18 10:07:29 -0700

    created routes to dashboard,savings,spending,savings,bills

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+// rfc command for react function
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Link to="/dashboard"></Link>
+      <h1>dashboard</h1>
+    </>
+  );
+}
