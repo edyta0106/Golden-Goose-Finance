74b1380555e06f0f5c1de627b87b93575208bfcc
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-18 11:50:08 -0700

    added box shadow to form component

--- client/src/components/Bills/Bills.js
+++ client/src/components/Bills/Bills.js
@@ -10,118 +10,118 @@ export default function Bills() {
     <>
       <Link to="/bills"></Link>
       {/* Form Container */}
-        <Container
+      <Container
+        sx={{
+          boxShadow: "0px 0px 20px 2px black",
+          width: "90%",
+          height: "100vh",
+          bgcolor: "#C5C6C7",
+          marginTop: "5%",
+          marginBottom: "5%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {/* Back Arrow */}
+        <Box
           sx={{
-            width: "90%",
-            height: "100vh",
-            bgcolor: "#C5C6C7",
-            marginTop: "5%",
-            marginBottom: "5%",
+            width: "105%",
+            display: "flex",
+            justifyContent: "flex-start",
+          }}
+        >
+          <Button>
+            <ArrowBack
+              sx={{
+                fontSize: 40,
+                color: "black",
+              }}
+            />
+          </Button>
+        </Box>
+        {/* Form Header Text Container */}
+        <Box
+          sx={{
+            height: "10%",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "200%",
+              fontWeight: "bolder",
+            }}
+          >
+            Bills
+          </Typography>
+        </Box>
+        {/* Form Header Total Amount Container */}
+        <Box
+          sx={{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-            overflow: "scroll",
+            paddingBottom: "7%",
           }}
         >
-          {/* Back Arrow */}
-          <Box
+          <Typography
             sx={{
-              width: "105%",
-              display: "flex",
-              justifyContent: "flex-start",
+              fontSize: "150%",
+              fontWeight: "bolder",
+              paddingTop: "7%",
+              paddingBottom: "15%",
             }}
           >
-            <Button>
-              <ArrowBack
-                sx={{
-                  fontSize: 40,
-                  color: "black",
-                }}
-              />
-            </Button>
-          </Box>
-          {/* Form Header Text Container */}
-          <Box
+            $321.45
+          </Typography>
+          <Button
+            variant="outlined"
             sx={{
-              height: "10%",
+              width: "150%",
             }}
           >
-            <Typography
-              sx={{
-                fontSize: "200%",
-                fontWeight: "bolder",
-              }}
-            >
-              Bills
-            </Typography>
-          </Box>
-          {/* Form Header Total Amount Container */}
-          <Box
+            Add A Bill
+          </Button>
+        </Box>
+        {/* Individual Bill Container */}
+        <Box
+          sx={{
+            width: "100%",
+            height: "15vh",
+            bgcolor: "#546E7A",
+            color: "white",
+            marginTop: "10px",
+            marginBottom: "10px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+            alignContent: "center",
+          }}
+        >
+          <Typography
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              paddingBottom: "7%",
+              fontSize: "130%",
+              fontWeight: "bold",
             }}
           >
-            <Typography
-              sx={{
-                fontSize: "150%",
-                fontWeight: "bolder",
-                paddingTop: "7%",
-                paddingBottom: "15%",
-              }}
-            >
-              $321.45
-            </Typography>
-            <Button
-              variant="outlined"
-              sx={{
-                width: "150%",
-              }}
-            >
-              Add A Bill
-            </Button>
-          </Box>
-          {/* Individual Bill Container */}
-          <Box
+            Jun. 16th
+          </Typography>
+          <Typography
             sx={{
-              width: "100%",
-              height: "15vh",
-              bgcolor: "#546E7A",
-              color: "white",
-              marginTop: "10px",
-              marginBottom: "10px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-around",
-              alignContent: "center",
+              fontSize: "150%",
             }}
           >
-            <Typography
-              sx={{
-                fontSize: "130%",
-                fontWeight: "bold",
-              }}
-            >
-              Jun. 16th
-            </Typography>
-            <Typography
-              sx={{
-                fontSize: "150%",
-              }}
-            >
-              Netflix
-            </Typography>
-            <Typography
-              sx={{
-                fontSize: "150%",
-              }}
-            >
-              $15.99
-            </Typography>
-          </Box>
-        </Container>
+            Netflix
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: "150%",
+            }}
+          >
+            $15.99
+          </Typography>
+        </Box>
+      </Container>
     </>
   );
 }
