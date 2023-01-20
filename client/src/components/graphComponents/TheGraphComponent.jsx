edc502811bd2b4aeb487cf8d2f2c641e5abc2520
Chris <cbega1227@gmail.com>
2023-01-20 11:55:31 -0700

    Cleaned comments

--- client/src/components/graphComponents/TheGraphComponent.jsx
+++ client/src/components/graphComponents/TheGraphComponent.jsx
@@ -4,28 +4,35 @@ import { LineGraph } from "./LineGraph";
 import { PieChart } from "./PieChart";
 import { Box, styled, Button, Grid, Paper } from "@mui/material";
 
-const StyledBox = styled(Box)({});
-const StyledButton = styled(Button)({
-  padding: "1rem 1.8rem",
-  backgroundColor: "#9db7e9",
-  color: "#fff",
-  marginTop: "5rem",
-  marginLeft: "1rem",
-  "&:hover": {
-    backgroundColor: "#5b88dc",
-  },
+const StyledBox = styled(Box)({
+  position: "relative",
 });
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#FAF9F6",
-  ...theme.typography.body2,
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+// const StyledButton = styled(Button)({
+//   padding: "1rem 1.8rem",
+//   backgroundColor: "#9db7e9",
+//   color: "#fff",
+//   marginTop: "5rem",
+//   marginLeft: "1rem",
+//   "&:hover": {
+//     backgroundColor: "#5b88dc",
+//   },
+// });
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: "#FAF9F6",
+//   ...theme.typography.body2,
+//   textAlign: "center",
+//   color: theme.palette.text.secondary,
+// }));
 export function TheGraphComponent({ chart }) {
   //1. Call use state - chart, setChart
   //2. build function that checks the values passed from button click
   //  a. call state method to update state, and rerender
   //3. return if.. else if... else value : pie , line , bar
+
+  // 1. call useState - store windowResize, setwindowResize
+  // 2. build function that checks what the windowResize is
+  //  a. call setWindowResize to update the size of chart, and rerender
+  //3. return if .. else if... else value : breakpoints
 
   return (
     <>
@@ -55,3 +62,5 @@ export function TheGraphComponent({ chart }) {
     </>
   );
 }
+
+// Important note : Responsive - https://www.chartjs.org/docs/latest/configuration/responsive.html#important-note
